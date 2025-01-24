d865d20adc6ef0d340ebb2d792bf1b2279e4d1a1
Hovhannes <hovhannes@ispras.ru>
2025-01-25 00:53:57 +0400

    Added KP_01 solutions

--- DP/KP.cpp
+++ DP/KP.cpp
@@ -70,6 +70,32 @@ int UKP_DP(const std::vector<Item> &items, const int weight) {
     return dp[weight];
 }
 
+int KP_01_rec(const std::vector<Item> &items, const int weight, int n) {
+    if (weight <= 0 || n == 0)
+        return 0;
+    if (items[n-1].weight > weight) {
+        return KP_01_rec(items, weight, n-1);
+    }
+    int res1 = KP_01_rec(items, weight, n-1);
+    int res2 = items[n-1].bonus + KP_01_rec(items, weight - items[n-1].weight, n-1);
+
+    return std::max(res1, res2);
+}
+
+int KP_01_DP(const std::vector<Item> &items, const int weight) {
+    std::vector<std::vector<int>> m(items.size() + 1, std::vector<int>(weight+1, 0));
+    for (size_t i = 1; i < m.size(); i++) {
+        for (size_t j = 1; j < weight + 1; j++) {
+            if (items[i-1].weight <= j) {
+                m[i][j] = std::max(m[i-1][j], m[i-1][j - items[i-1].weight] + items[i-1].bonus);
+            } else {
+                m[i][j] = m[i-1][j];
+            }
+        }
+    }
+    return m[m.size()-1][weight];
+}
+
 void test_UKP_rec(const std::vector<Item>& items, const int weight) {
     std::cout << UKP_rec(items, weight) << "\n";
 }
@@ -86,8 +112,7 @@ void test_UKP_DP(const std::vector<Item>& items, const int weight) {
     std::cout << UKP_DP(items, weight) << "\n";
 }
 
-
-int main() {
+void test_UKP() {
     const std::vector<Item> items = {
         // {1, 10},
         {3, 30},
@@ -98,4 +123,28 @@ int main() {
     test_UKP_memoization(items, weight);
     test_UKP_rec(items, weight);
     test_UKP_DP(items, weight);
+}
+
+void test_KP_01_rec(const std::vector<Item>& items, const int weight) {
+    std::cout << KP_01_rec(items, weight, items.size()) << "\n";
+}
+
+void test_KP_01_DP(const std::vector<Item>& items, const int weight) {
+    std::cout << KP_01_DP(items, weight) << "\n";
+}
+
+void test_KP_01() {
+    const std::vector<Item> items = {
+        {10, 60},
+        {20, 100},
+        {30, 120},
+    };
+    constexpr int weight = 50;
+    test_KP_01_rec(items, weight);
+    test_KP_01_DP(items, weight);
+}
+
+int main() {
+    test_UKP();
+    test_KP_01();
 }
